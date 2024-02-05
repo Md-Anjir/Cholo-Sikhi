93196469a10412dfe90bd3621c489be546159394
Md-Anjir <104209904+Md-Anjir@users.noreply.github.com>
2024-02-05 14:07:17 +0600

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.cholo_shikhi {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.cholo_shikhi to javafx.fxml;
-    exports com.example.cholo_shikhi;
-}
